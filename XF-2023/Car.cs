f1ea8c29a1e845ba328668ac73aaa3ea82465f20
Elev <Elev@CNCN-WS046>
2024-02-07 09:42:34 +0200

    Add project files.

---
+++ XF-2023/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF_2023
+{
+    public class Car : Vehicle
+    {
+
+        //Fields
+        private int carSeats;
+        //private string color;
+        //private string name;
+        //private int maxSpeed; //field 
+        //private int year;
+
+        //Properties
+        public int CarSeats { get; set; }
+        //public string  Name { //property
+        //    get { return name; }
+        //    set { name = value; } 
+        //}
+        //public string Color {
+        //    get { return color; }
+        //    set { color = value; }
+        //}
+
+        //public int Year {
+        //    get { return year; }
+        //    //set { year = value; }
+        //    set { 
+        //    if (value < 1980) {
+        //            throw new Exception("Please pass a correct value");
+        //        }
+        //    else {
+        //            year = value;
+        //        }
+        //    }
+        //}
+
+        //Constructors
+        public Car(string nameName, string colorName, int yearValue, int maxSpeedValue, int carSeatsNo) : base(nameName, colorName, yearValue, maxSpeedValue)
+        {
+            carSeats = carSeatsNo;
+        }
+
+        public Car(string nameName, string colorName, int yearValue, int maxSpeedValue, string eng_type, int carSeatsNo) : base(nameName, colorName, yearValue, maxSpeedValue, eng_type)
+        {
+            carSeats = carSeatsNo;
+        }
+        public Car(string nameName, int carSeatsNo) : base(nameName)
+        {
+            carSeats = carSeatsNo;
+        }
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + " is going at the speed of " + maxSpeed);
+        }
+
+    }
+}
